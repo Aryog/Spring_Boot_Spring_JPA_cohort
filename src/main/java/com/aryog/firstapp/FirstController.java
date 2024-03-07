3a429c724dc7df97275d2d9c271f474d25fb75c1
Aryog <yogesh000aryal@gmail.com>
2024-03-07 21:03:29 +0545

    query params and path variables

--- src/main/java/com/aryog/firstapp/FirstController.java
+++ src/main/java/com/aryog/firstapp/FirstController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello from my first controller";
-    }
+//    @GetMapping("/hello")
+//    public String sayHello(){
+//        return "Hello from my first controller";
+//    }
     @PostMapping("/post")
     public String post(@RequestBody String message){
         return "Request accepted and message is: "+message;
@@ -16,5 +16,23 @@ public class FirstController {
     @PostMapping("/post-order")
     public String post(@RequestBody Order order){
         return "Request accepted and message is: "+order.toString();
+    }
+    // Using the Record order
+    @PostMapping("/order-record")
+    public String postRecord(@RequestBody OrderRecord order){
+        return "Request accepted and message is: "+order.toString();
+    }
+
+    // http://localhost:8080/hello/yogesh
+    @GetMapping("/hello/{user-name}")
+    public String pathVar(@PathVariable("user-name") String userName){
+        return "my value = "+userName;
+    }
+
+    // http://localhost:8080/hello?param_name=paramvalue&param_name_1=value_2
+    @GetMapping("/hello")
+    public String paramVar(@RequestParam("user-name") String userName,
+                           @RequestParam("user-lastname") String userLastname){
+        return "my value = "+userName+" "+userLastname;
     }
 }
---
+++ src/main/java/com/aryog/firstapp/OrderRecord.java
@@ -0,0 +1,7 @@
+package com.aryog.firstapp;
+
+public record OrderRecord(String customerName,
+        String productName,
+        int quantity) {
+
+}
