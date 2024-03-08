19f8f6df690c2708a3a7b4a9281655c5aa1fe5ae
Aryog <yogesh000aryal@gmail.com>
2024-03-08 23:05:16 +0545

    custom methods in jpa repository

--- src/main/java/com/aryog/firstapp/FirstController.java
+++ src/main/java/com/aryog/firstapp/FirstController.java
@@ -1,16 +1,37 @@
 package com.aryog.firstapp;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class FirstController {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello from my first controller";
-    }
-    @PostMapping("/post")
-    public String post(@RequestBody String message){
-        return "Request accepted and message is: "+message;
+    private final StudentRepository repository;
+
+    public FirstController(StudentRepository repository) {
+        this.repository = repository;
     }
 
+    @PostMapping("/students")
+    public Student post(@RequestBody Student student){
+        return repository.save(student);
+    }
+    @GetMapping("/students")
+    public List<Student> findAllStudent(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/students/search/{student-name}")
+    public List<Student> findStudentsByName(
+            @PathVariable("student-name") String name
+    ){
+        return repository.findAllByFirstnameContaining(name);
+    }
+    @DeleteMapping("/students/{student-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(
+            @PathVariable("student-id") Integer id    ){
+        repository.deleteById(id);
+    }
 }
